af200ad8b6d19d614ba03b076ce3e6397d9af949
Isabela Rozani Floriano <irf161295@gmail.com>
2023-03-16 11:57:49 -0300

    feat: create the function that takes the five most popular news, popularity criterion is the amount of comments

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,17 @@
 # Requisito 10
+from tech_news.database import find_news
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    top_five_news = []
+    news_list = find_news()
+    news = sorted(news_list, key=lambda news_list: news_list["title"])
+    news_by_comments = sorted(news, key=lambda news: news["comments_count"])
+    news_by_comments.reverse()
+    top_news = news_by_comments[:5]
+    for news in top_news:
+        top_five_news.append((news["title"], news["url"]))
+    return top_five_news
 
 
 # Requisito 11
